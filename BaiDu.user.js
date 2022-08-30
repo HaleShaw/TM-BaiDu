bd76a7660434b9b43175a787242b25f6a3081a81
HaleShaw <HaleShaw@163.com>
2022-08-30 17:47:50 +0800

    update the style

--- BaiDu.user.js
+++ BaiDu.user.js
@@ -4,7 +4,7 @@
 // @description        Beautify the bookmark at the home page of Baidu.
 // @description:zh-CN  美化百度首页的书签页面。
 // @namespace          https://github.com/HaleShaw
-// @version            1.0.1
+// @version            1.0.2
 // @author             HaleShaw
 // @copyright          2020+, HaleShaw (https://github.com/HaleShaw)
 // @license            AGPL-3.0-or-later
@@ -181,6 +181,12 @@ a.site-item-img_3NN3N>img {
 /* 添加网址的弹窗中的“添加网址”按钮 */
 #desktopModalMask > div > div > div:nth-child(2) > div:nth-child(1) > button{
   margin-top: 10px !important;
+}
+
+/* 添加网址的弹窗中，分类下拉列表 */
+.select-wrapper_3Yt8t,
+.select-board_xSgU5 {
+  max-height: 300px !important;
 }
   `;
 
